884b86d08c9392558bdb5b6743d8c89210aa484d
Levminer <33373714+Levminer@users.noreply.github.com>
2022-05-24 22:30:15 +0200

    Background transparency

--- core/src/main.rs
+++ core/src/main.rs
@@ -10,6 +10,9 @@ mod os;
 mod password_encryption;
 mod warning;
 
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_mica, apply_vibrancy, NSVisualEffectMaterial};
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
@@ -20,6 +23,24 @@ fn main() {
       info::info,
       os::os
     ])
+    .setup(|app| {
+      let win = app.get_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&win, NSVisualEffectMaterial::AppearanceBased)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      #[cfg(target_os = "windows")]
+      apply_mica(&win).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+      win
+        .get_window("main")
+        .unwrap()
+        .set_decorations(true)
+        .unwrap();
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
